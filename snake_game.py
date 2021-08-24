c7c603c40cfaf06a174f1770263866793ca8407a
Maksym Ruchko <linkoln2896@gmail.com>
2021-08-24 11:37:09 +0300

    сделал игру без меню движение змейки считывается с клавиатуры и счет работает

--- snake_game.py
+++ snake_game.py
@@ -1,113 +1,137 @@
+import os
+import time
 import random
-
-class BodySnake:
-
-    def __init__(self):
-        self.body_snake = '*'
-
-    def add_body_snake(self):
-        return self.body_snake
-
-class Snake:
-
-    DEFAULT_MOVE_DIR = 'd'
-
-    def __init__(self, x=10, y=10):
-        self.head = '*'
-        self.body, self.body_x, self.body_y = [], [], []
-        self.x = x
-        self.y = y
-        self.dir = None
-        self.speed = 1
-        self.board = Board(width=40, height=20)
-        self.body_snake = BodySnake()
-
-    def move(self, apple):
-        self.dir = input('Выберете направлении змейки: ')
-
-        # движение ВЛЕВО
-        if self.dir in 'aA4фФ':
-            self.x -= self.speed
-        # движение ВПРАВО
-        elif self.dir in 'dD6вВ':
-            self.x += self.speed
-        # движение ВВЕРХ
-        elif self.dir in 'wW8цЦ':
-            self.y -= self.speed
-        # движение ВНИЗ
-        elif self.dir in 'sS2ыЫ':
-            self.y += self.speed
-        else:
-            print('')
-
-        if self.x == apple.x and self.y == apple.y:
-            apple.x = random.randint(2, self.board.width - 1)
-            apple.y = random.randint(2, self.board.height - 1)
-            self.body.append(self.body_snake.add_body_snake())
-        print(f'Съедино яблок: {len(self.body)}')
-
-    def add_body_snake(self):
-        return (len(self.body) + 1)
+from threading import Thread
 
 
-class Apple:
+if os.getenv("OS") in ["Windows_NT", "Linux"]:
+    import msvcrt as m
 
-    def __init__(self):
-        self.board = Board(width=40, height=20)
-        self.x = random.randint(2, self.board.width - 1)
-        self.y = random.randint(2, self.board.height - 1)
-        self.apple = 'A'
+last2X = 0
+last2Y = 0
+lastX = 0
+lastY = 0
+elemX = [0 for i in range(100)]
+elemY = [0 for i in range(100)]
 
-    def print_apple(self):
-        print(self.apple, end='')
-
-
-class Board:
-
-    def __init__(self, width=10, height=10):
-        self.width = width
-        self.height = height
-
-    def print_board(self, snake, apple):
-        for h in range(self.height):
-            for w in range(self.width):
-                if snake.x == w and snake.y == h:
-                    print(snake.head * snake.add_body_snake(), end='')
-                elif apple.x == w and apple.y == h:
-                    apple.print_apple()
-
-                if w == 0 or h == 0:
-                    print('#', end='')
-                elif h == self.height-1 or w == self.width-1:
-                    print('#', end='')
-                else:
-                    for s in range(len(snake.body)):
-                        snake.body_x.append(snake.x - 1)
-                        snake.body_y.append(snake.y - 1)
-                        if snake.body_x[s] == w - 1 and snake.x == apple.x and snake.body_y[s] == h - 1 \
-                                and snake.y == apple.y:
-                            # snake.body.insert(-1, (snake.x, snake.y))
-                            print(snake.head + snake.body[s], end='')
-                    print(' ', end='')
-            print()
 
 class Game:
 
-    def start_game(self):
-        board = Board(width=40, height=20)
-        snake = Snake(10, 10)
-        apple = Apple()
-
-        print('Snake Game')
-        print('Управление: WASD / 2468 / ЦФЫВ')
-        # print('Выход из игры: E или 5')
-
-        while not ((snake.x > board.width-1 or snake.x < 1) or (snake.y > board.height - 1 or snake.y < 1)):
-            board.print_board(snake, apple)
-            snake.move(apple)
-        else:
-            print('Game Over')
+    x = 5
+    y = 3
+    game_thread = True
+    fruit_cord_x = 5
+    fruit_cord_y = 6
+    button_default = "d"
+    score = 0
+    icon_player = "►"
+    tail = "o"
 
 
-game1 = Game()
-game1.start_game()
+    def board(self, width: int = 40, height: int = 20, pos_player_x: int = x, pos_player_y: int = y):
+        global elemX, elemY, last2X, last2Y, lastX, lastY
+        GameFunction().console_clear()
+        for h in range(height):
+            for w in range(width):
+                if pos_player_x == Game.fruit_cord_x and pos_player_y == Game.fruit_cord_y:
+                    Game.fruit_cord_x = random.randint(2, width-1)
+                    Game.fruit_cord_y = random.randint(2, height-1)
+                    Game.score += 1
+
+                for el in range(Game.score):
+                    if pos_player_x == elemX[el] and pos_player_y == elemY[el]:
+                        print(f'\nGAME OVER\nScore: {Game.score}')
+                        Game.game_thread = False
+                        exit()
+
+                if not (Game.x in range(width-39)) and not (Game.y in range(height-1)) or \
+                    not (Game.x in range(width-1)) and not (Game.y in range(height-19)):
+                    print(f'\nGAME OVER\nScore: {Game.score}')
+                    Game.game_thread = False
+                    exit()
+
+                if w == 0:
+                    print('#', end='')
+                elif h == 0:
+                    print('#', end='')
+                elif h == height-1:
+                    print('#', end='')
+                elif w == width-1:
+                    print('#', end='')
+                elif pos_player_x == w and pos_player_y == h:
+                    print(Game.icon_player, end='')
+                elif Game.fruit_cord_x == w and Game.fruit_cord_y == h:
+                    print("A", end='')
+                else:
+                    pr = True
+                    for ls in range(Game.score):
+                        if elemX[ls] == w and elemY[ls] == h:
+                            print(Game.tail, end="")
+                            pr = False
+                    if pr:
+                        print(' ', end='')
+            print()
+
+        print(f"X player: {pos_player_x} || Y player: {pos_player_y}")
+        print(f"Score: {Game.score}\n\nWASD / Стрелочки - перемещение\nESC - выйти")
+        lastX = pos_player_x
+        lastY = pos_player_y
+        if Game.score > 0:
+            for el in range(Game.score):
+                last2X = elemX[el]
+                last2Y = elemY[el]
+                elemX[el] = lastX
+                elemY[el] = lastY
+                lastX = last2X
+                lastY = last2Y
+
+    def btn_move(self):
+        while Game.game_thread:
+            Game.button_default = m.getch()[0]
+
+    def move(self):
+        while Game.game_thread:
+            if Game.button_default in ["", " "]:
+                Game.button_default = "d"
+            elif Game.button_default in ["w", 119, 230, 72]:
+                Game.y -= 1
+                Game.icon_player = "▲"
+            elif Game.button_default in ["a", 97, 228, 75]:
+                Game.x -= 1
+                Game.icon_player = "◄"
+            elif Game.button_default in ["s", 115, 235, 80]:
+                Game.y += 1
+                Game.icon_player = "▼"
+            elif Game.button_default in ["d", 100, 162, 77]:
+                Game.x += 1
+                Game.icon_player = "►"
+
+            elif Game.button_default in ["exit", 27]:
+                print("Вы покинули игру")
+                Game.game_thread = False
+                exit()
+
+            Game().board(pos_player_x=Game.x, pos_player_y=Game.y)
+
+            time.sleep(0.2)
+
+
+class GameFunction:
+
+    def console_clear(self):
+        for i in ["cls", "clear"]:
+            os.system(i)
+            break
+
+    def main(self):
+        Game().board()
+        Thread(target=Game().move).start()
+        Thread(target=Game().btn_move).start()
+
+    def menu(self):
+        pass
+
+
+if __name__ == '__main__':
+    GameFunction().console_clear()
+    GameFunction().main()
